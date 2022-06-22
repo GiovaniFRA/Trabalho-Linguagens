7853b0c337090b06efc89b1b470df6dc999bc3fc
GiovaniFRA <ferreiraalencoi1@gmail.com>
2022-06-22 00:01:38 -0300

    menu+drones

--- menudeescolha.js
+++ menudeescolha.js
@@ -1,54 +1,55 @@
 let cowboy = document.getElementById("cowboy").style;
-let posi=255;
+let posi=40;
 let cont=0;
 document.addEventListener("keydown",tecla);
 
 function tecla(event){
     let Tec=event.keyCode;
    
-    if(Tec==40 && posi<450){
-     posi=posi+60;
+    if(Tec==40 && posi<80){
+     posi=posi+10;
      console.log(posi);
-     cowboy.top=posi+"px";
+     cowboy.top=posi+"%";
     }
 
-    if(Tec==38 && posi>255){
-        posi=posi-60;
+    if(Tec==38 && posi>40){
+        posi=posi-10;
         console.log(posi);
-        cowboy.top=posi+"px";
+        cowboy.top=posi+"%";
        }
 
     if(Tec==32){
             function shot(){
                 document.getElementById("bullet1").id="bullet";
-                document.getElementById("bullet").style.top=posi+50+"px";
+                document.getElementById("bullet").style.top=posi+8+"%";
                
             }
             shot();
-
             
-
-            function shotE(){document.getElementById("bullet").id="bullet1";}
             
             cont++
        }
 
-       if(cont==1){
+       if(cont==1 && posi==40){
+        function newTab(){window.location.assign("Pag1.html");}
+        setTimeout(newTab,1300);
+        }
+       if(cont==1 && posi==50){
         function newTab(){window.location.assign("Pag2.html");}
         setTimeout(newTab,1300);
         }
 
-        if(cont==1 && posi==170){
+        if(cont==1 && posi==60){
             function newTab(){window.location.assign("Pag3.html");}
             setTimeout(newTab,1300);
         }
 
-        if(cont==1 && posi==290){
+        if(cont==1 && posi==70){
             function newTab(){window.location.assign("Pag4.html");}
             setTimeout(newTab,1300);
             }
         
-        if(cont==1 && posi==410){
+        if(cont==1 && posi==80){
                 function newTab(){window.location.assign("Pag5.html");}
                 setTimeout(newTab,1300);
                 }
